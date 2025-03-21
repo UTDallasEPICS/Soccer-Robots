575eed270763649fc2acf0021e256cd4a0415858
MahdMalik <mahdmahdmalik@gmail.com>
2025-03-21 03:31:53 -0500

    all website code commented

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -5,14 +5,20 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
+//I believe this code sets the sruser cookie for the player
 export default defineEventHandler(async event => {
   event.context.prisma = prisma
+  //try to get security token first
   const srtoken = getCookie(event, 'srtoken') || ''
   if(srtoken){
+    //try to verify the 
     try {
+      //get the decoded srtoken cookie into claims
       const claims:any = jwt.verify(srtoken, fs.readFileSync(process.cwd() + '/cert-dev.pem'))
+      //if claims exists and it was a number only used once, do the following
       if(claims instanceof Object && "nonce" in claims){
         event.context.claims = claims
+        //get Id from the srtoken, and check if it's in prisma
         const id = claims['sub']
         const player = await prisma.player.findUnique({
           where:{
@@ -23,18 +29,23 @@ export default defineEventHandler(async event => {
             role: true
           } 
         })
+        //if able to find the player, set the sruser cookie, otherwise leave blank
         if(player){
           setCookie(event, 'sruser', JSON.stringify(player))
         } else {
           setCookie(event, 'sruser', '')
         }
       }
-    } catch (error) {
+    }
+    //when fails to decode srtoken cookie. In such a case try to redirect them to login 
+    catch (error) {
       console.error(error)
       setCookie(event, 'srtoken', '')
       return await sendRedirect(event, loginRedirectUrl())
     }
-  } else {
+  }
+  //if srtoken doesn't exist, set blank cookies 
+  else {
     setCookie(event, 'sruser', '')
     setCookie(event, 'srtoken', '')
   }
