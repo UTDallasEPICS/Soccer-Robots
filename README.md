dc701d225249c3c2187cebae266c1ab752436b54
Mahd Malik <mahdmahdmalik@gmail.com>
2025-03-29 21:43:48 -0500

    our code works with the website now

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 
 ## Backtracking.py
 
-## Server2.py
-
-## NewServer.py
-
 ## BackupTag.py
+
+NOTE: when attempting to run the servers, you'll need to activate the virtual
+environment (which includes things like the libraries used). To activate this,
+find the folder called "my-env". Inside that directory is another folder called "bin",
+and inside of that is a file called activate. You need to call "source activate" to activate
+the virtual environment.
+
+Also, make sure when communicating with the server that both the controller and GM server (on the
+raspberry pi) are active. If you're just testing the GM server, it will appear to not work unless
+the controller server is also active.
+
+Anyways here's how we actually run this and have it be connected with the website
+First, in demo-scripts take "ControllerPi.py", and run it. Move it to the backgroun
+(while its still running) and then run GmServerPi.py, and run that.
+
+Next, on the website do the npm run bootcontrol, npm run bootgame, npm run dev and stuff. Now,
+get two people to start the game, and profit as you see the updating timer and score GRAHHHH.
--- demo-scripts/GmServerPi.py
+++ demo-scripts/GmServerPi.py
@@ -11,68 +11,70 @@ PORT = 1234
 game_time = 0
 
 def getTime():
-	return game_time
+    return game_time
 runBackTracking = False
 
 async def serverGM(websocket, path):
-	while True:
-		game_time = 0
-		team1Score = 0
-		team2Score = 1
-		isReady = False 
-		
-		print("inside GM")
-		while isReady==False:
-			received_data = await websocket.recv()
-			received = json.loads(received_data)
-			print(received)
-			if received["type"] =="CHECK_READY":
-				isReady = True
-				await websocket.send(json.dumps({
-					"type": "IS_READY",
-					"payload": True
-				}))
-				break
-			else:
-				print("NOT READY")
+    while True:
+        game_time = 0
+        team1Score = 0
+        team2Score = 1
+        isReady = False 
 
-		received_data = await websocket.recv()
-		received = json.loads(received_data)    
-		print(received["payload"], type(received["payload"]))
-		game_time = received["payload"]["timer"]
-		print("\nTimer:")
+        print("inside GM")
+        while isReady==False:
+            received_data = await websocket.recv()
+            received = json.loads(received_data)
+            print(received)
+            if received["type"] =="CHECK_READY":
+                print("READY GRAHHHHH")
+                isReady = True
+                await websocket.send(json.dumps({
+                    "type": "IS_READY",
+                    "payload": True
+                }))
+                break
+            else:
+                print("Supposed to receive ready signal, what we actually got: " + received["type"])
+                print("NOT READY")
 
-		score_update = {
-			"type": "SCORE_UPDATE",
-			"payload": {"score1": team1Score, "score2": team2Score}
-		}
+        received_data = await websocket.recv()
+        received = json.loads(received_data)    
+        print(received["payload"], type(received["payload"]))
+        game_time = received["payload"]["timer"]
+        print("\nTimer:")
+        
+        score_update = {
+            "type": "SCORE_UPDATE",
+            "payload": {"score1": team1Score, "score2": team2Score}
+        }
 
-		while game_time >= 0 and isReady:
-			if game_time == 0:
-				final_score_update = {"type": "GAME_END","payload": {"timer": 0, "score1": team1Score, "score2": team2Score}}
-				await websocket.send(json.dumps(final_score_update))
-				isReady = False
-				runBackTracking = True
-				break
-			
-			if game_time%7==0:
-				team1Score += 1
-			elif game_time%9==0:
-				team2Score += 1
-			
-			print("Send Current Time: "+str(game_time))
-			await websocket.send(json.dumps({"type": "SCORE_UPDATE", "payload": {"score1": team1Score, "score2": team2Score}}))
-			await websocket.send(json.dumps({"type":"TIMER_UPDATE","payload":{"timer":game_time}}))
-			time.sleep(1)
-			game_time -= 1
-			
-		print("\n\n")
-		print("FINAL SCORE: Team 1: ", team1Score, " Team 2: ", team2Score)
-	
-		if runBackTracking:
-			test = runTrakcer()
-			runBackTracking = False
-		
+        while game_time >= 0 and isReady:
+            if game_time == 0:
+                final_score_update = {"type": "GAME_END","payload": {"timer": 0, "score1": team1Score, "score2": team2Score}}
+                await websocket.send(json.dumps(final_score_update))
+                isReady = False
+                runBackTracking = True
+                break
+
+            if game_time%7==0:
+                team1Score += 1
+            elif game_time%9==0:
+                team2Score += 1
+
+            print("Send Current Time: "+str(game_time))
+            await websocket.send(json.dumps({"type": "SCORE_UPDATE", "payload": {"score1": team1Score, "score2": team2Score}}))
+            await websocket.send(json.dumps({"type":"TIMER_UPDATE","payload":{"timer":game_time}}))
+            time.sleep(1)
+            game_time -= 1
+
+        print("\n\n")
+        print("FINAL SCORE: Team 1: ", team1Score, " Team 2: ", team2Score)
+
+        if runBackTracking:
+            test = runTrakcer()
+            runBackTracking = False
+
 start_server = websockets.serve(serverGM, HOST, PORT)
 print("STARTED GM SERVER")
 asyncio.get_event_loop().run_until_complete(start_server)
