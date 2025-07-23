c40571c217551b321d3207b01dd48e8bc53e3c49
khaledalshiddi <khaledalshiddi@gmail.com>
2025-07-23 13:23:50 -0500

    bash to run the files

---
+++ start_soccer_robot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Move to the project root
+cd "$(Soccer-Robots "$0")" || {
+  echo "Could not move into project directory"
+  exit 1
+}
+
+
+mkdir logs -p
+
+# Start EspManager.py
+echo "Starting EspManager.py"
+nohup python scripts/EspManager.py &>logs/esp.txt  &
+
+# Wait for sockets to initialize
+sleep 2
+
+# Start GmServerPi.py
+echo "Starting GmServerPi.py"
+nohup python scripts/GmServerPi.py &>logs/GMServer.txt &
+
+sleep 2
+
+# Start ControllerPi.py
+echo "Starting ControllerPi.py."
+nohup python scripts/ControllerPi.py &>logs/Controller.txt &
+
